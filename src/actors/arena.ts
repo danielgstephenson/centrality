19bcda380ff9499c7fed03c9ddb9cc1da010c5e1
danielgstephenson <dgstephenson.econ@gmail.com>
2025-06-09 01:39:02 -0400

    arena

--- src/actors/arena.ts
+++ src/actors/arena.ts
@@ -3,7 +3,7 @@ import { Simulation } from '../simulation'
 import { Actor } from './actor'
 
 export class Arena extends Actor {
-  static size = 10
+  static size = 20
   vertices: Vec2[]
   fixture: Fixture
 
